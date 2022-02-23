1fc49ae1ef5d3bdb7a8ca178d043638f0e62e8a0
bbouhtat <purplehazehz@gmail.com>
2022-02-23 18:53:30 +0100

    migration

---
+++ database/migrations/2022_02_23_174456_create_answer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->integer('question_id');
+            $table->integer('user_id');
+            $table->boolean('active');
+            $table->timestamps('created');
+            $table->timestamps('updated');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer');
+    }
+};
