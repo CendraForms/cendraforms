8c57ff3c7f2ef8a0b282e2d1df238bf2fe5e04a2
SEOAlexRamon <seoalexramon@gmail.com>
2022-03-07 15:09:05 +0100

    Code formating changes

--- database/migrations/2022_02_23_154250_create_roles_table.php
+++ database/migrations/2022_02_23_154250_create_roles_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-
+            
             $table->string('name');
             $table->boolean('active')->default(true);
-
+            
             $table->timestamps();
         });
     }
