4167027b3de4401ac47d4835ed626268e900c798
eimantas <milkintaseimantas@gmail.com>
2022-03-16 18:36:55 +0100

    merge

---
+++ resources/js/Shared/Answer/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-5">
+    <QuestionNumber
+      v-if="type == 'number'"
+      :value="content.value"
+      :min="content.min"
+      :max="content.max"
+    />
+  </div>
+</template>
+
+<script setup>
+import QuestionNumber from './Number'
+
+defineProps({
+  type: String,
+  content: Object,
+})
+</script>
