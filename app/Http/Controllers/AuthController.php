4959aaac2ef65397710600d19ad79cac044aaccc
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-03-16 15:50:54 +0100

    ara si, va de debo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,26 +6,34 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Str;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function socialCallbackGitlab()
     {
-        $user = Socialite::driver('gitlab')->user();
-
-        if (!Str::endsWith($user->getEmail(), '@cendrassos.net'))
+        try
         {
-            return redirect()->route('auth')->with('error', 'Usuari no autoritzat. Recorda utilitzar el correu del centre.');
+            $user = Socialite::driver('gitlab')->user();
+
+            if (!Str::endsWith($user->getEmail(), '@cendrassos.net'))
+            {
+                return redirect()->route('auth')->with('error', 'Usuari no autoritzat. Recorda utilitzar el correu del centre.');
+            }
+
+            $user = User::firstOrCreate([
+                "name" => str_replace('@cendrassos.net', '', $user->getEmail()),
+                "email" => $user->getEmail(),
+            ]);
+
+            Auth::login($user);
+
+            return redirect()->route('dashboard');
         }
-
-        $user = User::firstOrCreate([
-            "name" => str_replace('@cendrassos.net', '', $user->getEmail()),
-            "email" => $user->getEmail(),
-        ]);
-
-        Auth::login($user);
-
-        return redirect()->route('dashboard');
+        catch (Exception $exception)
+        {
+            return redirect('/accedir')->with('error', 'Error. Has d\'acceptar per poder accedir.');
+        }
     }
 }
