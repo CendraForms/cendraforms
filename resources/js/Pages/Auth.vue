b7d55d46638f589d5d1c1fc98bd20a30304a5ad3
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-03-16 18:36:45 +0100

    Feature-Laravel-Socialite-OAuth-GitLab-#180

--- resources/js/Pages/Auth.vue
+++ resources/js/Pages/Auth.vue
@@ -38,7 +38,6 @@
       <a
         class="flex items-center gap-3 w-full h-14 px-3 bg-white rounded-lg hover:bg-gray-100"
         href="/accedir/gitlab"
-        as="button"
       >
         <img class="w-8" src="/logos/gitLab.png" draggable="false" />
         <span>Accedir amb <b>GitLab</b></span>
