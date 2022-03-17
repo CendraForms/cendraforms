08d1d9953abe3a30827c184a9de46ccd9eb6d7d9
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-03-17 17:04:42 +0100

    proves gitlab

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
             return redirect()->route('auth');
         }
 
-        //dd(Socialite::driver($provider)->redirect());
+        dd(Socialite::driver($provider)->redirect());
     }
 
     public function socialCallback($provider)
