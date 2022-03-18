7fcffda1572603b9e3046318dfc4232ea61e66de
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-03-18 15:34:47 +0100

    funcionara?

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,14 +22,14 @@ class AuthController extends Controller
 
         if (!in_array($provider, $providers))
         {
-            return redirect()->route('auth');
+            return redirect()->route('auth.callback');
         }
     }
 
     public function socialCallback($provider)
     {
-        // try
-        // {
+        try
+        {
             $providers = ["google", "discord", "github", "gitlab"];
 
             if (!in_array($provider, $providers))
@@ -52,10 +52,10 @@ class AuthController extends Controller
             Auth::login($user);
 
             return redirect()->route('form.create');
-        // }
-        // catch (Exception $exception)
-        // {
-        //     return redirect('/accedir')->with('error', 'Error. Has d\'acceptar per poder accedir.');
-        // }
+        }
+        catch (Exception $exception)
+        {
+            return redirect('/accedir')->with('error', 'Error. Has d\'acceptar per poder accedir.');
+        }
     }
 }
