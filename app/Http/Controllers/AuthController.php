98e7d308a0caac5ff232db08fa8f703974082b3b
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-03-21 15:02:32 +0100

    socialite de gitlab

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,6 @@ class AuthController extends Controller
         }
         catch (Exception $exception)
         {
-            dd($exception);
             return redirect('/accedir')->with('error', 'Error. Has d\'acceptar per poder accedir.');
         }
     }
