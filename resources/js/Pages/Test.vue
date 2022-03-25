971c5edd8633607bebeafb0475b5842c8ef32f70
SEOAlexRamon <seoalexramon@gmail.com>
2022-03-25 15:12:22 +0100

    Added Create/Form view

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,11 @@
+<template>
+  {{ llista }}<br>
+  <Asd v-model:llista="llista" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Asd from "../Shared/Asd.vue";
+
+const llista = ref("Hola");
+</script>
---
+++ resources/js/Shared/Asd.vue
@@ -0,0 +1,26 @@
+<template>
+  {{ llista }}<br>
+  
+  <button @click="afegir">
+    Afegir
+  </button>
+</template>
+
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const props = defineProps({
+  llista: Array,
+});
+
+const emit = defineEmits([
+  "update:llista",
+]);
+
+const llista = ref(props.llista);
+
+const afegir = () => {
+  llista.value = "Adeu";
+  emit("update:llista", llista.value);
+}
+</script>
